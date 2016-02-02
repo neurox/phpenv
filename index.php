a39fec9c4806fdc32edc40ae1c7a61facbc6dbbf
NEUROX GOMEZ <NEUROX GOMEZ>
2016-02-02 11:59:04 -0600

    Added my first files!

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "Neurox Gómez";
+
+?>
+<!DOCTYPE html>
+<html>
+<meta lang="es">
+<head>
+	<title>Vagrant VM</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+	        <h1 class="titulo">Hola <?php echo $name; ?><br>
+	            Bienvenido a Vagrant VM<br>
+	            <span class="subtitulo">Mi primer sitio</span>
+	        </h1>
+	    </div>
+    </div>
+</body>
+</html>
